db56c69cc25751ed92be2ad562933332003fb99e
charan271271 <96542861+charan271271@users.noreply.github.com>
2022-10-06 22:34:42 +0530

    Update Single EC2.tf

--- Single EC2.tf
+++ Single EC2.tf
@@ -1,18 +1,23 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
-  name = "single-instance"
+  required_version = ">= 1.2.0"
+}
 
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "ExampleAppServerInstance"
   }
 }
